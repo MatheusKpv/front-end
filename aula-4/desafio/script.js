5d98c309f2677710c2357019e59f6fe2a8a4ce16
MatheusKpv <matheusmatias1910@gmail.com>
2024-09-12 18:37:46 -0300

    finalizando desafio aula 4

--- aula-4/desafio/script.js
+++ aula-4/desafio/script.js
@@ -1,14 +1,51 @@
-// let listaItens = [];
+let contTotal = 0;
+let contFinalizadas = 0;
 function adicionaItem(event) {
     event.preventDefault();
-    console.log('teste')
     const item = document.getElementById('includeItem').value;
     const li = document.createElement('li');
     document.getElementById('idLista').appendChild(li);
-    li.innerHTML = `<label>${item}</label><button></button>`;
+    li.innerHTML = `<label>${item}</label><button onclick='concluiItem(this)'><i class='fa fa-check'></i></button><button onclick='deletaItem(this)'><i class='fa fa-trash'></i></button>`;
     document.getElementById('includeItem').value = '';
+    document.getElementById('includeBtn').disabled = true;
+
+    contTotal++;
+    alteraContador();
 }
 function verificaBotao() {
-    console.log(document.getElementById('includeItem').value);
-    document.getElementById('includeBtn').disabled = false;
+    if (document.getElementById('includeItem').value.trim().length == 0) {
+        document.getElementById('includeBtn').disabled = true;
+    } else {
+        document.getElementById('includeBtn').disabled = false;
+    }
+}
+function deletaItem(botao) {
+    botao.parentElement.remove();
+
+    contTotal--;
+    alteraContador();
+}
+function concluiItem(botao) {
+    const label = botao.parentElement.querySelector('label');
+    label.style.color = 'grey';
+    label.style.textDecoration = 'line-through';
+    label.classList.add('concluida')
+    botao.disabled = true;
+
+    contFinalizadas++;
+    alteraContador();
+}
+function limpaTarefasConcluidas() {
+    let tarefas = document.querySelectorAll('.concluida');
+    tarefas.forEach(tarefa => {
+        tarefa.parentElement.remove();
+        contTotal--;
+        contFinalizadas--;
+    });
+    alteraContador();
+}
+
+function alteraContador() {
+    document.getElementById('finalizadas').innerText = contFinalizadas;
+    document.getElementById('total').innerText = contTotal;
 }
