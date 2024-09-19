503aa5ec90493f99c766ff5b87f4ed826d063df2
MatheusKpv <matheusmatias1910@gmail.com>
2024-09-19 11:59:16 -0300

    adicionando FormGroup

--- semana-2/aula-3/atividades/src/app/main/usuario/features/cadastro/cadastro.component.ts
+++ semana-2/aula-3/atividades/src/app/main/usuario/features/cadastro/cadastro.component.ts
@@ -2,43 +2,69 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Usuario } from '../../models/usuario';
 import { UsuarioService } from '../../services/usuario.service';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.scss'
+  styleUrl: './cadastro.component.scss',
 })
 export class CadastroComponent implements OnInit {
-  nomeInput: string = '';
-  emailInput: string = '';
-  foneInput: string = '';
+  formulario!: FormGroup;
   usuarioEdit?: Usuario = undefined;
 
-  constructor(private usuarioService: UsuarioService, private router: Router, private route: ActivatedRoute) { };
+  constructor(
+    private usuarioService: UsuarioService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private formBuider: FormBuilder
+  ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(p => {
+    this.initForm();
+    this.route.params.subscribe((p) => {
       const paramId = p['id'];
       if (paramId) {
         this.usuarioEdit = this.usuarioService.findById(paramId);
         if (this.usuarioEdit) {
-          this.nomeInput = this.usuarioEdit.nome;
-          this.emailInput = this.usuarioEdit.email;
-          this.foneInput = this.usuarioEdit.fone;
+          this.formulario.patchValue({
+            nome: this.usuarioEdit.nome,
+            email: this.usuarioEdit.email,
+            fone: this.usuarioEdit.fone,
+          });
         } else {
           this.router.navigate(['/usuario/listagem']);
         }
-      };
-    })
+      }
+    });
   }
 
+  initForm() {
+    this.formulario = this.formBuider.group({
+      nome: [null,[ Validators.required, this.validaNome]],
+      email: [null, [Validators.required, Validators.email]],
+      fone: [null, Validators.required],
+    });
+  }
+  validaNome(control: AbstractControl): ValidationErrors | null {
+    if(control.value) {
+      console.log(control.value.length);
+      if (control.value.length < 2) {
+        return {nomeValido : true}
+      } else {
+        return null
+      }
+    } else {
+      return null
+    }
+  }
   cadastra() {
     const usuario: Usuario = {
       id: this.usuarioEdit ? this.usuarioEdit.id : undefined,
-      nome: this.nomeInput,
-      email: this.emailInput,
-      fone: this.foneInput
-    }
+      nome: this.formulario.value.nome,
+      email: this.formulario.value.email,
+      fone: this.formulario.value.fone,
+    };
     if (this.usuarioEdit) {
       this.usuarioService.atualiza(usuario);
       this.usuarioEdit = undefined;
@@ -47,4 +73,42 @@ export class CadastroComponent implements OnInit {
     }
     this.router.navigate(['/usuario/listagem']);
   }
+
+  // nomeInput: string = '';
+  // emailInput: string = '';
+  // foneInput: string = '';
+  // usuarioEdit?: Usuario = undefined;
+
+  // constructor(private usuarioService: UsuarioService, private router: Router, private route: ActivatedRoute) { };
+
+  // ngOnInit(): void {
+  //   this.route.params.subscribe(p => {
+  //     const paramId = p['id'];
+  //     if (paramId) {
+  //       this.usuarioEdit = this.usuarioService.findById(paramId);
+  //       if (this.usuarioEdit) {
+  //         this.nomeInput = this.usuarioEdit.nome;
+  //         this.emailInput = this.usuarioEdit.email;
+  //         this.foneInput = this.usuarioEdit.fone;
+  //       } else {
+  //         this.router.navigate(['/usuario/listagem']);
+  //       }
+  //     };
+  //   })
+  // }
+  // cadastra() {
+  //   const usuario: Usuario = {
+  //     id: this.usuarioEdit ? this.usuarioEdit.id : undefined,
+  //     nome: this.nomeInput,
+  //     email: this.emailInput,
+  //     fone: this.foneInput
+  //   }
+  //   if (this.usuarioEdit) {
+  //     this.usuarioService.atualiza(usuario);
+  //     this.usuarioEdit = undefined;
+  //   } else {
+  //     this.usuarioService.cadastra(usuario);
+  //   }
+  //   this.router.navigate(['/usuario/listagem']);
+  // }
 }
